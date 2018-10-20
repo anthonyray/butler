8da70080cc44d2bdf26642390db76a4dd43783ec
Anthony Reinette <dantho361@hotmail.com>
2018-10-20 23:45:39 +0200

    adds markdown parser as a dependeency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [selmer "1.12.2"]]
+                 [selmer "1.12.2"]
+                 [markdown-clj "1.0.4"]]
   :main ^:skip-aot butler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
