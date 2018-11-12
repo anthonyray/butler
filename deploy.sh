0af7e01f2303db1012b7aa5b709b6a60dd34c56e
anthonyray <dantho361@hotmail.com>
2018-11-12 21:10:35 +0100

    missing commands from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
+BUTLER_VERSION="$(cat project.clj | head -n 1 | cut -f 3 -d' ' | tr -d '"')"
+echo "Deploying Butler ${BUTLER_VERSION}"
+cat stub.sh target/uberjar/butler-"$BUTLER_VERSION"-standalone.jar > butler && chmod +x butler
 cp butler /$HOME/Applications/butler/
 rm butler
