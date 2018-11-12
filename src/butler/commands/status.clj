0e41bb606bc7a849402d2c3fe0c9f1c20d241e9a
anthonyray <dantho361@hotmail.com>
2018-11-12 21:32:53 +0100

    draft of the status subcommand

--- src/butler/commands/status.clj
+++ src/butler/commands/status.clj
@@ -1,6 +1,13 @@
 (ns butler.commands.status)
 
+(def api-root-url "https://api.github.com/")
+(def api-repos-url  "repos")
+(def repository-owner "anthonyray")
+(def repository-name "anthonyray.github.io")
+(def repository-url
+  (str api-root-url "/" api-repos-url "/" repository-owner "/" repository-name))
+
 (defn run
   [& args]
-  (println "Status command is invoked")
+  (slurp repository-url)
   0)
